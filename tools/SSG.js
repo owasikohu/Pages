0076646cb660a04f94229f4836d666c5ece8c895
owasikohu <owatas2022@gmail.com>
2024-09-24 23:51:24 +0900

    eiygujuvvfjgitghtjd

--- tools/SSG.js
+++ tools/SSG.js
@@ -31,16 +31,18 @@ fs.readFile(inputPath, 'utf8', (err, data) => {
 <html lang="ja">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <script src="https://page.owasikohu.com/lib/js/script.js"></script>
     <link rel="stylesheet" href="https://page.owasikohu.com/lib/css/header.css">
     <link rel="stylesheet" href="https://page.owasikohu.com/lib/css/footer.css">
     <link rel="stylesheet" href="https://page.owasikohu.com/lib/css/style.css">
     <link rel="stylesheet" href="https://page.owasikohu.com/lib/css/github.css">
+    
     <link rel="shortcut icon" href="https://page.owasikohu.com/favicon.ico" type="image/x-icon">
     <title>${fileName}</title>
 </head>
 <body>
-    <header id="header"><div class="header-inner"><a class="header-logo" href="https://page.owasikohu.com/"><img src="https://page.owasikohu.com/lib/img/logo.png" width="200" height="50"></a><div class="header-site-menu"><nav class="site-menu"><ul class="menu-group"><li class="menu-item"><a href="https://page.owasikohu.com/profile">/profile</a></li><li class="menu-item"><a href="https://page.owasikohu.com/blog">/blog</a></li><li class="menu-item"><a href="https://page.owasikohu.com/projects">/projects</a></li><li class="menu-item"><a href="https://page.owasikohu.com/webring">/webring</a></li><li class="menu-item"><a href="https://page.owasikohu.com/misc">/misc</a></li></ul></nav></div></div><hr></header>
-    ${htmlContent}
+    <div class="header-inner"><a class="header-logo" href="https://page.owasikohu.com"><img src="https://page.owasikohu.com/lib/img/logo.png" width="200" height="50"></a><div class="header-site-menu"><div class="hamburger-menu" id="hamburger-menu">☰</div><nav class="site-menu" id="site-menu"><ul class="menu-group"><li class="menu-item"><a href="https://page.owasikohu.com/profile">/profile</a></li><li class="menu-item"><a href="https://page.owasikohu.com/blog">/blog</a></li><li class="menu-item"><a href="https://page.owasikohu.com/projects">/projects</a></li><li class="menu-item"><a href="https://page.owasikohu.com/webring">/webring</a></li><li class="menu-item"><a href="https://page.owasikohu.com/misc">/misc</a></li></ul></nav></div></div><hr>
+        ${htmlContent}
     <footer id="footer"><hr><span id="copyright">Copyright (C) 2024 owasikohu. Some rights reserved.</span></footer>
 </body>
 </html>
