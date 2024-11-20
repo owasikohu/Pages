8f775ac345b0f940880fb38571a4f78925d079df
owasikohu <owatas2022@gmail.com>
2024-11-20 17:58:16 +0900

    update ssg

--- ssg.ts
+++ ssg.ts
@@ -1,4 +1,26 @@
+import { marked } from "marked"
+import { Glob } from "bun"
+import path from "path"
 
+const glob = new Glob("**/*.md");
+
+for await (const file of glob.scan(".")) {
+    const filename: string = file.split("/").pop();
+    const outoutpath: string = path.join(path.dirname(file), "index.html")
+    const md: string = await readfile(file)
+    const html: string = md2html(md,filename)
+    await writefile(html,outoutpath)
+}
+
+async function readfile (filepath) {
+    const file = Bun.file(filepath)
+    return await file.text()
+}
+
+function md2html (md,filename) {
+    const name = removeExtension(filename)
+    const html = marked.parse(md)
+    const template = `
     <!DOCTYPE html>
     <html lang="ja">
     <head>
@@ -8,21 +30,22 @@
     <link rel="stylesheet" href="https://page.owasikohu.com/lib/css/style.css">
     <link rel="stylesheet" href="https://page.owasikohu.com/lib/css/github.css">
     <link rel="shortcut icon" href="https://page.owasikohu.com/favicon.ico" type="image/x-icon">
-    <title>owasikohu.com - home</title>
+    <title>owasikohu.com - ${name}</title>
     </head>
     <body>
     <header id="header"><div class="header-inner"><a class="header-logo" href="https://page.owasikohu.com/"><img src="https://page.owasikohu.com/lib/img/logo.png" width="200" height="50"></a><div class="header-site-menu"><nav class="site-menu"><ul class="menu-group"><li class="menu-item"><a href="https://page.owasikohu.com/profile">/profile</a></li><li class="menu-item"><a href="https://page.owasikohu.com/blog">/blog</a></li><li class="menu-item"><a href="https://page.owasikohu.com/projects">/projects</a></li><li class="menu-item"><a href="https://page.owasikohu.com/webring">/webring</a></li><li class="menu-item"><a href="https://page.owasikohu.com/misc">/misc</a></li></ul></nav></div></div><hr></header>
-    <h2>このサイトについて</h2>
-<p>ようこそ。ここには様々なものを置いています。</p>
-<p>まだサイトは発展途上なので、表示が乱れるかもしれません。</p>
-<p>astroでこのサイトを再度構築するかも</p>
-<h2>変更履歴</h2>
-<p>2024/9/25 目標だったすべてのサイトを設置</p>
-<p>2024/9/25 技術的な問題によりpage.owasikohu.comからweb.owasikohu.comに変更</p>
-<p>2024/9/24 javascriptをなるべく使用しない実装に変更</p>
-<p>2024/9/24 現在のサイトにリニューアル</p>
-
+    ${html}
     <footer id="footer"><hr><span id="copyright">Copyright (C) 2024 owasikohu. Some rights reserved.</span></footer>
     </body>
     </html>
-    
+    `
+    return template
+}
+
+async function writefile (html,outoutpath) {
+    await Bun.write(outoutpath, html)
+}
+
+function removeExtension(fileName) {
+    return fileName.substring(0, fileName.lastIndexOf('.')) || fileName;
+}
